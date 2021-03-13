2df1df683be01c0b62b35abfacc4925053cf757f
irina-grabhermeier <igr7222@students.fhv.at>
2021-03-13 09:52:20 +0100

    finished exercise 1 of assignment 1

---
+++ Assignment1-Template/Exercise1/Phonebook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise1
+{
+    class Phonebook
+    {
+        private Dictionary<string, string> _phonebook;
+        private string _name;
+        public Phonebook(string name)
+        {
+            _name = name;
+            _phonebook = new Dictionary<string, string>();
+        }
+
+        public string GetPhoneNumber(string name)
+        {
+            if (_phonebook.ContainsKey(name))
+            {
+                return _phonebook[name];
+            }
+            else
+            {
+                return "invalid";
+            }
+        }
+
+        public void AddEntry(string name, string phonenumber)
+        {
+            _phonebook.Add(name, phonenumber);
+        }
+    }
+}
---
+++ Assignment1-Template/Exercise1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var isRunning = true;
+            var phonebook = new Phonebook("Dornbirn");
+
+            while (isRunning)
+            {
+                Console.WriteLine("Write the name you are looking for: ");
+                string nameInput = Console.ReadLine();
+                if (nameInput == "quit")
+                {
+                    isRunning = false;
+                    Environment.Exit(0);
+                }
+                CheckResult(phonebook.GetPhoneNumber(nameInput), phonebook, nameInput);
+
+            }
+        }
+
+        private static void CheckResult(string result, Phonebook phonebook, string nameInput)
+        {
+            if (result == "invalid")
+            {
+                Console.WriteLine("Please enter your phone number: ");
+                var phoneInput = Console.ReadLine();
+                phonebook.AddEntry(nameInput, phoneInput);
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
